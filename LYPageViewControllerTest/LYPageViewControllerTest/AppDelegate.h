893f8a2053d9a00dd2f9787514c065d615196ecc
li_yong <371191322@qq.com>
2016-09-13 16:15:39 +0800

    修改人：李勇 bug单号： 修改内容：添加新工程文件

---
+++ LYPageViewControllerTest/LYPageViewControllerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYPageViewControllerTest
+//
+//  Created by li_yong on 16/9/13.
+//  Copyright © 2016年 li_yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYPageViewControllerTest/LYPageViewControllerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYPageViewControllerTest
+//
+//  Created by li_yong on 16/9/13.
+//  Copyright © 2016年 li_yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
